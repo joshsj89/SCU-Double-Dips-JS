eac95e073f1458ff994625082f029b83033df78a
joshsj89 <joshsj89@gmail.com>
2023-04-15 02:54:18 -0700

    Added get_cores()
    get_cores() fetches the names and IDs of each core requirement/pathway, so that it always gets the right ones for each quarter. (Previously E_NTSC, F_MATH, and I_PTHPP were missing.

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const prompt = require('prompt-sync')({sigint: true});
 // 4360 is the code for Summer Quarter 2022.
 // 4400 is the code for Fall Quarter 2022.
 // 4420 is the code for Winter Quarter 2023.
-// 4430 is the code for Spring Quarter 2023.
+// 4440 is the code for Spring Quarter 2023.
 
 /*
 
@@ -42,7 +42,27 @@ const quarterNameMap = {
     "Fall 2021": "fall2021",
     "Summer 2021": "summer2021"
 }
-  
+
+const get_cores = async (quarterCode) => {
+    const headers = {
+        "authority": "www.scu.edu",
+        "accept": "*/*",
+        "x-requested-with": "XMLHttpRequest",
+        "user-agent": process.env.USER_AGENT,
+        "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "sec-gpc": "1",
+        "origin": "https://www.scu.edu",
+        "sec-fetch-site": "same-origin",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-dest": "empty",
+        "accept-language": "en-US,en;q=0.9",
+    };
+    
+    const response = await axios.get(`https://www.scu.edu/apps/ws/courseavail/autocomplete/${quarterCode}/pathways`, {headers});
+    const data = response.data;
+    return data;
+}
+
 const get_courses = async (core, quarterCode) => {
     const payload = `newcore=${core}&maxRes=10000`;
     const headers = {
@@ -100,52 +120,14 @@ const validateInput = async () => {
     } 
     
     const courses = {};
+    const coreDict = {};
 
-    // Core Codes
-    const coreDict = {
-        "I_PTHAMS": "American Studies (Pathway)",
-        "I_PTHAE": "Applied Ethics (Pathway)",
-        "I_PTHB": "Beauty (Pathway)",
-        "I_PTHCHD": "Children, Family, & Society (Pathway)",
-        "I_PTHCINST": "Cinema Studies (Pathway)",
-        "I_PTHDEM": "Democracy (Pathway)",
-        "I_PTHDT": "Design Thinking (Pathway)",
-        "I_PTHFHP": "Feeding the World (Pathway)",
-        "I_PTHGGE": "Gender, Globalization & Empire (Pathway)",
-        "I_PTHGSB": "Gender, Sexuality & the Body (Pathway)",
-        "I_PTHGB": "Global Health (Pathway)",
-        "I_PTHHR": "Human Rights (Pathway)", 
-        "I_PTHIS": "Islamic Studies (Pathway)",
-        "I_PTHJA": "Justice and the Arts (Pathway)",
-        "I_PTHLSJ": "Law & Social Justice (Pathway)",
-        "I_PTHLPOSC": "Leading People, Org & Soc Chng (Pathway)",
-        "I_PTHPS": "Paradigm Shifts (Pathway)",
-        "I_PTHPR": "Politics & Religion (Pathway)",
-        "I_PTHRPSI": "Race Place & Soc Inequalities (Pathway)",
-        "I_PTHS": "Sustainability (Pathway)",
-        "I_PTHDA": "The Digital Age (Pathway)",
-        "I_PTHVST": "Values Science Technology (Pathway)",
-        "I_PTHV": "Vocation (Pathway)",
-        "I_AW": "Advanced Writing",
-        "E_ARTS": "Arts",
-        "E_CE": "Civic Engagement",
-        "F_CTW1": "Critical Thinking and Writing 1",
-        "F_CTW2": "Critical Thinking and Writing 2",
-        "F_CI1": "Cultures and Ideas 1",
-        "F_CI2": "Cultures and Ideas 2",
-        "E_CI3": "Cultures and Ideas 3",
-        "E_DV": "Diversity",
-        "E_ETH": "Ethics",
-        "I_EL": "Experiential Learning for Social Justice",
-        "F_RTC1": "Religion Theology & Culture 1",
-        "E_RTC2": "Religion Theology & Culture 2",
-        "E_RTC3": "Religion Theology & Culture 3",
-        "E_STS": "Science Technology & Society",
-        "E_SOSC": "Social Science",
-        "E_ARTSPAR": "Partial Credit Arts",
-        "E_CEPAR": "Partial Credit Civic Engagement",
-        "E_STSPAR": "Partial Credit Engineering, Math, CS"
-    };
+    // Fetch core requirements/pathways and add it to object
+    const data = await get_cores(quarterMap[quarter]);
+
+    for (const info of data.results) {
+        coreDict[info['value']] = info['label'];
+    }
 
     for (const core in coreDict) {
         console.log(`Fetching ${coreDict[core]} . . .`)
